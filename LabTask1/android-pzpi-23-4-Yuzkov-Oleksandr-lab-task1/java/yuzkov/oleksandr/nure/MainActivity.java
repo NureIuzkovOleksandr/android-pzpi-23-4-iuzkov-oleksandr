c17307285bf4074005d45b056a1b1570463e5b50
Yuzkov Oleksandr <oleksandr.iuzkov@nure.ua>
2025-01-31 15:41:28 +0100

    Add files via upload

--- LabTask1/android-pzpi-23-4-Yuzkov-Oleksandr-lab-task1/java/yuzkov/oleksandr/nure/MainActivity.java
+++ LabTask1/android-pzpi-23-4-Yuzkov-Oleksandr-lab-task1/java/yuzkov/oleksandr/nure/MainActivity.java
@@ -2,73 +2,48 @@ package yuzkov.oleksandr.nure;
 
 import android.os.Bundle;
 import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String ActivityTag = "ActivityLifecycle";
+    private static final String TAG = "ActivityLifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(ActivityTag, "Activity created");
-
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        configureInsets();
-    }
-
-    private void configureInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
-            Insets systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            view.setPadding(
-                    systemBarsInsets.left,
-                    systemBarsInsets.top,
-                    systemBarsInsets.right,
-                    systemBarsInsets.bottom
-            );
-            return insets;
-        });
+        Log.d(TAG, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(ActivityTag, "Activity started");
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(ActivityTag, "Activity resumed");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
-        Log.i(ActivityTag, "Activity paused");
         super.onPause();
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
-        Log.i(ActivityTag, "Activity stopped");
         super.onStop();
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
-        Log.i(ActivityTag, "Activity destroyed");
         super.onDestroy();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(ActivityTag, "Activity restarted");
+        Log.d(TAG, "onDestroy");
     }
 }
--- PractTask1/android-pzpi-23-4-Yuzkov-Oleksandr-pract-task1/java/yuzkov/oleksandr/nure/MainActivity.java
+++ PractTask1/android-pzpi-23-4-Yuzkov-Oleksandr-pract-task1/java/yuzkov/oleksandr/nure/MainActivity.java
@@ -1,47 +1,31 @@
 package yuzkov.oleksandr.nure;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txt;
-    private Button btn1;
-    private EditText editTxt;
+    private TextView textView;
+    private Button button;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        btn1 = findViewById(R.id.pressBtn);
-        txt = findViewById(R.id.txt1);
-        editTxt = findViewById(R.id.editText);
-        btn1.setOnClickListener(new View.OnClickListener(){
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txt.setText(editTxt.getText());
+                String originalText = textView.getText().toString();
+                String reversedText = new StringBuilder(originalText).reverse().toString();
+                textView.setText(reversedText);
             }
         });
-
     }
 }
